4d6cce1d042e1b35c522b2a60ed86f49e824abdc
Simon <phillipe.simon@gmail.com>
2024-01-29 18:57:40 -0300

    Organizing the project

--- App.tsx
+++ App.tsx
@@ -1,25 +1,7 @@
-import { Text, View } from "react-native";
+import Home from "./src/screens/Home";
 
-export default function App(){
+export default function App() {
   return(
-    <View style={{
-      flex: 1,
-      backgroundColor: '#131016',
-    }} >
-    <Text style={{
-      color: '#f0f0f0',
-      fontSize: 24,
-      fontWeight: 'bold',
-      textAlign: 'center',
-      margin: 48,
-    }} >Nome do evento</Text>
-    <Text style={{
-       color: '#f0f0f0',
-       fontSize: 16,
-       fontWeight: 'bold',
-       textAlign: 'center',
-       margin: 48,
-    }}>Domingo, 4 de Novembro de 2024</Text>
-    </View>
+    <Home/>
   )
 }
